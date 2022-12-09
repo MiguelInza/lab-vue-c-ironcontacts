9d17151cdc3f7c2e1442c9ca5364d2faa2fedb94
Miguel Inza <inza.miguel@gmail.com>
2022-12-09 21:23:42 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-
-  <i class="fas fa-trophy"></i>
+  <h1>IronContacts</h1>
+  <button @click="addContact()" type="button">Add Random Contact</button>
+  <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(contact, index) of contacts" :key="index">
+            <!--<td>{{ index }}</td>-->
+            <td><img :src="contact.pictureUrl" :alt="contact.name" width="70" /></td>
+            <td>{{ contact.name }}</td>
+            <td>{{ contact.popularity }}</td>
+            <td>{{ won(contact.wonOscar) }}</td>
+            <td>{{ won(contact.wonEmmy) }}</td>
+          </tr>
+        </tbody>
+  </table>
 </template>
 
 <script>
-export default {
+import CONTACTS from "./contacts.json";
 
+export default {
+  data() {
+    return {
+      contacts: CONTACTS.slice(0,5),
+    };
+  },
+  methods: {
+    won(data) {
+      if (data) {
+        return "YES"
+      }
+      else return "NO"
+    },
+    addContact() {
+      this.contacts.push(CONTACTS[Math.ceil(Math.random()*CONTACTS.length)])      
+    },
+  },  
 }
+
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
